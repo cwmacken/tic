257c7d398697c7ea80db336d75b4d587eab85122
cwmacken <cwmacken@gmail.com>
2013-09-30 19:28:24 -0700

    moved all game play to one page

--- js/main.js
+++ js/main.js
@@ -1,13 +1,41 @@
+
+var p1Name = 'Player 1';
+var p2Name = 'PLayer 2';
+
+
+
 function strGame(){
-		window.location.href="game.html"
-		}
-function home(){
-	window.location.href="index.html"
+	document.getElementById('game').style.display="inline";
+	document.getElementById('startPage').style.display="none";
+  var p1Hold = document.getElementById('p1').value;
+  var p2Hold = document.getElementById('p2').value;
+  document.getElementById('x').innerHTML = (p1Hold.toUpperCase())+'\'S';
+  p1Name = p1Hold.toUpperCase();
+  p2Name = p2Hold.toUpperCase();
+	return p1Name, p2Name;
+  }
+
+function reset(){
+  var lookup=['a','b','c','d','e','f','g','h','i'];
+
+  for(var z=0;z<=8;z++){
+   
+        document.getElementById(lookup[z]).value=0;
+        document.getElementById(lookup[z]).innerHTML="";
+        document.getElementsByClassName('alertBox')[0].style.display = "none";
+        document.getElementById('o').innerHTML = "TURN";
+        turn = 1
+
+
+  };
 }
 
-var p1Name = document.getElementById('p1').value;
-var p2Name = document.getElementById('p2').value;
-
+function home(){
+	document.getElementById('game').style.display="none";
+	document.getElementById('startPage').style.display="inline";
+			
+}
+// document.getElementById('x').innerHTML = p1Name;
 var turn =1;
 
 
@@ -18,48 +46,48 @@ function playBox(){
 					case 1:
 						event.target.innerHTML = "X";
 						event.target.value = 1;
-						document.getElementById('x').innerHTML = p2Name;
+						document.getElementById('x').innerHTML = p2Name+'\'S';
 						break;
 					case 2:
 						event.target.innerHTML = "0";
 						event.target.value = -1;
-						document.getElementById('x').innerHTML = p1Name;
+						document.getElementById('x').innerHTML = p1Name+'\'S';
 						break;
 					case 3:
 						event.target.innerHTML = "X";
 						event.target.value = 1;
-						document.getElementById('x').innerHTML = p2Name;
+						document.getElementById('x').innerHTML = p2Name+'\'S';
 						break;
 					case 4:
 						event.target.innerHTML = "0";
 						event.target.value = -1;
-						document.getElementById('x').innerHTML = p1Name;
+						document.getElementById('x').innerHTML = p1Name+'\'S';
 						break;
 					case 5:
 						event.target.innerHTML = "X";
 						event.target.value = 1;
-						document.getElementById('x').innerHTML = p2Name;
+						document.getElementById('x').innerHTML = p2Name+'\'S';
 						break;
 					case 6:
 						event.target.innerHTML = "0";
 						event.target.value = -1;
-						document.getElementById('x').innerHTML = p1Name;
+						document.getElementById('x').innerHTML = p1Name+'\'S';
 						break;
 					
 					case 7:
 						event.target.innerHTML = "X";
 						event.target.value = 1;
-						document.getElementById('x').innerHTML = p2Name;
+						document.getElementById('x').innerHTML = p2Name+'\'S';
 						break;
 					case 8:
 						event.target.innerHTML = "0";
 						event.target.value = -1;
-						document.getElementById('x').innerHTML = P1Name;
+						document.getElementById('x').innerHTML = p1Name+'\'S';
 						break;
 					case 9:
 						event.target.innerHTML = "X";
 						event.target.value = 1;
-						document.getElementById('x').innerHTML = P2Name;
+						document.getElementById('x').innerHTML = p2Name+'\'S';
 						break;
 
 
@@ -127,7 +155,7 @@ function check(){
 	  		document.getElementById('x').innerHTML = "";
 	  		document.getElementById('o').innerHTML = "";
 	  		document.getElementsByClassName('alertBox')[0].style.display = "inline";
-	  		document.getElementsByClassName('alertText')[0].innerHTML = "X WINS!!!";
+	  		document.getElementsByClassName('alertText')[0].innerHTML = p1Name+" WINS!!!";
 	  		setTimeout("strGame();",3000);
 	  	}
 	 else if(
@@ -192,186 +220,186 @@ function check(){
 
 // -----------------------------ALT CODE FOR PICKING O -------------------------------------------------
 
-function strGameO(){
-				window.location.href="gameO.html"
-		}
-function home(){
-	window.location.href="index.html"
-}
+// function strGameO(){
+// 				window.location.href="gameO.html"
+// 		}
+// function home(){
+// 	window.location.href="index.html"
+// }
 
-var turnO =1;
+// var turnO =1;
 
 
-function playBoxO(){
-	if(event.target.innerHTML==""){
-		switch(turnO)
-				{	
-					case 1:
-						event.target.innerHTML = "O";
-						event.target.value = 1;
-						document.getElementById('x').innerHTML = "X's";
-						break;
-					case 2:
-						event.target.innerHTML = "X";
-						event.target.value = -1;
-						document.getElementById('x').innerHTML = "O's";
-						break;
-					case 3:
-						event.target.innerHTML = "O";
-						event.target.value = 1;
-						document.getElementById('x').innerHTML = "X's";
-						break;
-					case 4:
-						event.target.innerHTML = "X";
-						event.target.value = -1;
-						document.getElementById('x').innerHTML = "O's";
-						break;
-					case 5:
-						event.target.innerHTML = "O";
-						event.target.value = 1;
-						document.getElementById('x').innerHTML = "X's";
-						break;
-					case 6:
-						event.target.innerHTML = "X";
-						event.target.value = -1;
-						document.getElementById('x').innerHTML = "O's";
-						break;
+// function playBoxO(){
+// 	if(event.target.innerHTML==""){
+// 		switch(turnO)
+// 				{	
+// 					case 1:
+// 						event.target.innerHTML = "O";
+// 						event.target.value = 1;
+// 						document.getElementById('x').innerHTML = "X's";
+// 						break;
+// 					case 2:
+// 						event.target.innerHTML = "X";
+// 						event.target.value = -1;
+// 						document.getElementById('x').innerHTML = "O's";
+// 						break;
+// 					case 3:
+// 						event.target.innerHTML = "O";
+// 						event.target.value = 1;
+// 						document.getElementById('x').innerHTML = "X's";
+// 						break;
+// 					case 4:
+// 						event.target.innerHTML = "X";
+// 						event.target.value = -1;
+// 						document.getElementById('x').innerHTML = "O's";
+// 						break;
+// 					case 5:
+// 						event.target.innerHTML = "O";
+// 						event.target.value = 1;
+// 						document.getElementById('x').innerHTML = "X's";
+// 						break;
+// 					case 6:
+// 						event.target.innerHTML = "X";
+// 						event.target.value = -1;
+// 						document.getElementById('x').innerHTML = "O's";
+// 						break;
 					
-					case 7:
-						event.target.innerHTML = "O";
-						event.target.value = 1;
-						document.getElementById('x').innerHTML = "X's";
-						break;
-					case 8:
-						event.target.innerHTML = "X";
-						event.target.value = -1;
-						document.getElementById('x').innerHTML = "O's";
-						break;
-					case 9:
-						event.target.innerHTML = "O";
-						event.target.value = 1;
-						document.getElementById('x').innerHTML = "X's";
-						break;
+// 					case 7:
+// 						event.target.innerHTML = "O";
+// 						event.target.value = 1;
+// 						document.getElementById('x').innerHTML = "X's";
+// 						break;
+// 					case 8:
+// 						event.target.innerHTML = "X";
+// 						event.target.value = -1;
+// 						document.getElementById('x').innerHTML = "O's";
+// 						break;
+// 					case 9:
+// 						event.target.innerHTML = "O";
+// 						event.target.value = 1;
+// 						document.getElementById('x').innerHTML = "X's";
+// 						break;
 
 
-				}
-			}
-		else{
+// 				}
+// 			}
+// 		else{
 
-			turnO= turnO-1
-		}	
-		turnO = turnO+1
-}
-
-
-function checkO(){
+// 			turnO= turnO-1
+// 		}	
+// 		turnO = turnO+1
+// }
 
 
-		var a = document.getElementById('a').value;
-		var b = document.getElementById('b').value;
-		var c  = document.getElementById('c').value;
+// function checkO(){
 
-		var d = document.getElementById('d').value;
-		var e = document.getElementById('e').value;
-		var f = document.getElementById('f').value;
 
-		var g = document.getElementById('g').value;
-		var h = document.getElementById('h').value;
-		var i = document.getElementById('i').value
+// 		var a = document.getElementById('a').value;
+// 		var b = document.getElementById('b').value;
+// 		var c  = document.getElementById('c').value;
 
-	if(
-		(
-			(a+b+c==3) 
-		) 
-			|| 
-		(
-			(d+e+f==3)
-		)
-			|| 
-	  	(
-	  		(g+h+i==3) 
-	  	)
-	  		||
-	  	(
-	  		(a+d+g==3)
-	  	) 	
-	  		|| 
-	  	(
-	  		(b+e+h==3) 
-	  	)
-	  		|| 
-		( 
-			(c+f+i==3)
-		) 
-			||
-		(
-			(a+e+i==3)
-		) 
-			|| 
-		(
-			(c+e+g==3) 
-		)
-	)
-		{
+// 		var d = document.getElementById('d').value;
+// 		var e = document.getElementById('e').value;
+// 		var f = document.getElementById('f').value;
+
+// 		var g = document.getElementById('g').value;
+// 		var h = document.getElementById('h').value;
+// 		var i = document.getElementById('i').value
+
+// 	if(
+// 		(
+// 			(a+b+c==3) 
+// 		) 
+// 			|| 
+// 		(
+// 			(d+e+f==3)
+// 		)
+// 			|| 
+// 	  	(
+// 	  		(g+h+i==3) 
+// 	  	)
+// 	  		||
+// 	  	(
+// 	  		(a+d+g==3)
+// 	  	) 	
+// 	  		|| 
+// 	  	(
+// 	  		(b+e+h==3) 
+// 	  	)
+// 	  		|| 
+// 		( 
+// 			(c+f+i==3)
+// 		) 
+// 			||
+// 		(
+// 			(a+e+i==3)
+// 		) 
+// 			|| 
+// 		(
+// 			(c+e+g==3) 
+// 		)
+// 	)
+// 		{
 	  		
-	  		document.getElementById('x').innerHTML = "";
-	  		document.getElementById('o').innerHTML = "";
-	  		document.getElementsByClassName('alertBox')[0].style.display = "inline";
-	  		document.getElementsByClassName('alertText')[0].innerHTML = "O WINS!!!";
-	  		setTimeout("strGame();",3000);
-	  	}
-	 else if(
-		(
-			(a+b+c==-3)  
-		) 
-			|| 
-		(
-			(d+e+f==-3) 
-		)
-			|| 
-	  	(
-	  		(g+h+i==-3)
-	  	)
-	  		||
-	  	(
-	  		(a+d+g==-3) 
-	  	) 	
-	  		|| 
-	  	(
-	  		(b+e+h==-3) 
-	  	)
-	  		|| 
-		( 
-			(c+f+i==-3) 
-		) 
-			||
-		(
-			(a+e+i==-3) 
-		) 
-			|| 
-		(
-			(c+e+g==-3) 
-		)
-	)
-		{
-	  		document.getElementById('x').innerHTML = "";
-	  		document.getElementById('o').innerHTML = "";
-	  		document.getElementsByClassName('alertBox')[0].style.display = "inline";
-	  		document.getElementsByClassName('alertText')[0].innerHTML = "X WINS!!!";
-	  		setTimeout("strGame();",3000);
-	  	}
+// 	  		document.getElementById('x').innerHTML = "";
+// 	  		document.getElementById('o').innerHTML = "";
+// 	  		document.getElementsByClassName('alertBox')[0].style.display = "inline";
+// 	  		document.getElementsByClassName('alertText')[0].innerHTML = "O WINS!!!";
+// 	  		setTimeout("strGame();",3000);
+// 	  	}
+// 	 else if(
+// 		(
+// 			(a+b+c==-3)  
+// 		) 
+// 			|| 
+// 		(
+// 			(d+e+f==-3) 
+// 		)
+// 			|| 
+// 	  	(
+// 	  		(g+h+i==-3)
+// 	  	)
+// 	  		||
+// 	  	(
+// 	  		(a+d+g==-3) 
+// 	  	) 	
+// 	  		|| 
+// 	  	(
+// 	  		(b+e+h==-3) 
+// 	  	)
+// 	  		|| 
+// 		( 
+// 			(c+f+i==-3) 
+// 		) 
+// 			||
+// 		(
+// 			(a+e+i==-3) 
+// 		) 
+// 			|| 
+// 		(
+// 			(c+e+g==-3) 
+// 		)
+// 	)
+// 		{
+// 	  		document.getElementById('x').innerHTML = "";
+// 	  		document.getElementById('o').innerHTML = "";
+// 	  		document.getElementsByClassName('alertBox')[0].style.display = "inline";
+// 	  		document.getElementsByClassName('alertText')[0].innerHTML = "X WINS!!!";
+// 	  		setTimeout("strGame();",3000);
+// 	  	}
 
 
-	 else{
-	 	if(turnO==10){
-	 		document.getElementById('x').innerHTML = "";
-	  		document.getElementById('o').innerHTML = "";
-	  		document.getElementsByClassName('alertBox')[0].style.display = "inline";
-	  		document.getElementsByClassName('alertText')[0].innerHTML = "DRAW...";
-	  		setTimeout("strGame();",3000);
-	 	}
-	 }
-}
+// 	 else{
+// 	 	if(turnO==10){
+// 	 		document.getElementById('x').innerHTML = "";
+// 	  		document.getElementById('o').innerHTML = "";
+// 	  		document.getElementsByClassName('alertBox')[0].style.display = "inline";
+// 	  		document.getElementsByClassName('alertText')[0].innerHTML = "DRAW...";
+// 	  		setTimeout("strGame();",3000);
+// 	 	}
+// 	 }
+// }
 
 
 
